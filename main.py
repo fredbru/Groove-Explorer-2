de53a7c81e6f4fc444aa44258a5dfff1e9b279f3
Fred Bruford <fred.bruford@gmail.com>
2021-03-22 18:39:14 +0000

    Added tabs for static explorer and customisable explorer. can switch freely between the two nicely

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 import numpy as np
 import pandas as pd
-import time
 from MusicSOM.MusicSOM import *
-import sys
 import random
-import pydeck
-from bokeh.layouts import column
-from bokeh.plotting import figure, output_file, show, curdoc
-from bokeh.models import ColumnDataSource, Button, TextInput
+from bokeh.layouts import column, row
+from bokeh.plotting import figure
+from bokeh.models import ColumnDataSource, Panel, Tabs
 from bokeh.models.tools import HoverTool, TapTool, PointDrawTool
 from bokeh.models.callbacks import CustomJS
 from bokeh.events import PanEnd
@@ -15,6 +12,9 @@ from bokeh.models import Button
 from bokeh.io import curdoc
 from sklearn import preprocessing
 
+
+np.set_printoptions(suppress=True, precision=6)
+np.set_printoptions(edgeitems=50, linewidth=100000)
 
 # To run: bokeh serve --show run-bokeh.py
 
@@ -26,33 +26,6 @@ def text_input_handler(attr, old, new):
     except ValueError:
         print("Please enter a valid number")
 
-def pan_python_callback():
-
-    for i in range(94):
-        if source.data['X'][i] != groove_map_info['X'][i]:
-            old_coordinates = [int(round(groove_map_info['X'][i],0)),
-                               int(round(groove_map_info['Y'][i],0))]
-
-            new_X = round(source.data['X'][i], 0) + round(random.uniform(-0.2, 0.2), 2)
-            new_Y = round(source.data['Y'][i], 0) + round(random.uniform(-0.2, 0.2), 2)
-            #source.patch({'X': [(i, new_X)], 'Y': [(i, new_Y)]})
-            groove_map_info.at[i, 'X'] = new_X
-            groove_map_info.at[i, 'Y'] = new_Y
-
-            new_coordinates = [int(round(new_X, 0)), int(round(new_Y, 0))]
-            print("Groove = ", source.data['GrooveName'][i])
-            print("New coordinates = ", new_coordinates)
-            print("Old coordinates =", old_coordinates)
-
-            groove = features[i]
-            som.update_active_learning_nurnberger_local(groove, new_coordinates, old_coordinates)
-    groove_map_info.update(pd.DataFrame(get_winners(features, names, som, palette_names),
-                                                columns=['GrooveName', 'PaletteName', 'X', 'Y', 'Colour']))
-    for i in range(94):
-        new_X = groove_map_info['X'][i]
-        new_Y = groove_map_info['Y'][i]
-        source.patch({'X': [(i, new_X)], 'Y': [(i, new_Y)]})
-    print('Done')
 
 def make_node_locations(dim):
     xNodePoints = np.arange(dim+1)
@@ -104,12 +77,13 @@ def get_winners(features, names, som, palette_names):
         im = im+1
     return winners
 
-def setup_SOM(palette_folder, dim):
-    combinedLabels = np.load('Groove-Explorer-2/Mixed_8x12_Unweighted' + 'Names.npy')
+def setup_SOM(data, dim):
+    path = 'Groove-Explorer-2/'
+    combinedLabels = np.load(path+data + 'Names.npy')
     names = combinedLabels[0]
     #palette_names = os.listdir('/home/fred/BFD/python/grooves/' + sys.argv[1] + '/')
     palette_names = combinedLabels[1]
-    features = np.load('Groove-Explorer-2/Mixed_8x12_Unweighted' + ".npy")
+    features = np.load(path+data + ".npy")
     features = features.astype(np.float32)
     a = features
     # b = (a - np.min(a)) / np.ptp(a) #this doesn't seem to work...?
@@ -135,78 +109,104 @@ def regenerate_SOM(num_iterations=20):
     print('Done')
 
 
-TAPCODE = """
-var alldata = source.data;
-var selected = source.selected.indices;
-console.log(selected)
-var groovename = alldata['GrooveName'][selected[0]];
-var filetype = ".mp3";
-var directory = "Groove-Explorer-2/static/Mixed-8x12-MP3/";
-var file = directory.concat(groovename, filetype);
-console.log(groovename)
-if (groovename != "undefined")
-{
-    var audio = new Audio(file);
-    audio.play();
-    console.log("Playing " + groovename + "...");
-    }
-"""
-np.set_printoptions(suppress=True, precision=6)
-np.set_printoptions(edgeitems=50, linewidth=100000)
+def make_explorer(data_file='Part_1_Unweighted', explorer_type='Customised'):
+    PLAY_AUDIO_EXPLORER = """
+    var alldata = source.data;
+    var selected = source.selected.indices;
+    console.log(selected)
+    var groovename = alldata['GrooveName'][selected[0]];
+    var filetype = ".mp3";
+    var directory = "Groove-Explorer-2/static/ALL/";
+    var file = directory.concat(groovename, filetype);
+    console.log(groovename)
+    if (groovename != "undefined")
+    {
+        var audio = new Audio(file);
+        audio.play();
+        console.log("Playing " + groovename + "...");
+        }
+    """
 
-dim = 12
-som, features, names, palette_names = setup_SOM(palette_folder,dim)
-print(features)
-print("\n \n")
-#som.trainCPU(features, num_iterations=150000)
-som.weights = np.load("Groove-Explorer-2/SOM_Weights_MLR_3M.npy")
+    def go_back():
+        som.revert_active_learning()
+        groove_map_info.update(pd.DataFrame(get_winners(features, names, som, palette_names),
+                                            columns=['GrooveName', 'PaletteName', 'X', 'Y', 'Colour']))
+        for i in range(94):
+            new_X = groove_map_info['X'][i]
+            new_Y = groove_map_info['Y'][i]
+            source.patch({'X': [(i, new_X)], 'Y': [(i, new_Y)]})
 
-groove_map_info = pd.DataFrame(get_winners(features, names, som, palette_names), columns=['GrooveName',
-                                                                'PaletteName', 'X', 'Y', 'Colour'])
-source = ColumnDataSource(groove_map_info)
+    def pan_python_callback():
+        for i in range(94):
+            if source.data['X'][i] != groove_map_info['X'][i]:
+                old_coordinates = [int(round(groove_map_info['X'][i], 0)),
+                                   int(round(groove_map_info['Y'][i], 0))]
 
-hover= HoverTool()
-hover.tooltips=[
-    ('Name', '@GrooveName'),
-    ('Palette', '@PaletteName'),
-]
+                new_X = round(source.data['X'][i], 0) + round(random.uniform(-0.2, 0.2), 2)
+                new_Y = round(source.data['Y'][i], 0) + round(random.uniform(-0.2, 0.2), 2)
+                # source.patch({'X': [(i, new_X)], 'Y': [(i, new_Y)]})
+                groove_map_info.at[i, 'X'] = new_X
+                groove_map_info.at[i, 'Y'] = new_Y
 
-TOOLS = "crosshair, pan, wheel_zoom"
-explorer = figure(x_range
-                  =(-1,dim), y_range=(-1,dim), tools=TOOLS, title='Groove Explorer 2')
-#explorer.image(image=[1.0-som.distance_map()], x=-0.5,y=-0.5, dw=dim, dh=dim, palette="Greys256", level='image')
-tap = TapTool()
-tap.callback=CustomJS(code=TAPCODE, args=dict(source=source))
-explorer.add_tools(hover)
-explorer.add_tools(tap)
+                new_coordinates = [int(round(new_X, 0)), int(round(new_Y, 0))]
+                print("Groove = ", source.data['GrooveName'][i])
+                print("New coordinates = ", new_coordinates)
+                print("Old coordinates =", old_coordinates)
 
-renderer = explorer.circle(source=source, x='X', y='Y', color='Colour',fill_alpha=0.6, size=15,
-         hover_fill_color='yellow', hover_alpha=1, nonselection_alpha=0.6)
+                groove = features[i]
+                som.update_active_learning_nurnberger_local(groove, new_coordinates, old_coordinates)
+        groove_map_info.update(pd.DataFrame(get_winners(features, names, som, palette_names),
+                                            columns=['GrooveName', 'PaletteName', 'X', 'Y', 'Colour']))
+        for i in range(94):
+            new_X = groove_map_info['X'][i]
+            new_Y = groove_map_info['Y'][i]
+            source.patch({'X': [(i, new_X)], 'Y': [(i, new_Y)]})
+        print('Done')
 
-point_drag = PointDrawTool(renderers=[renderer], add=False)
-explorer.add_tools(point_drag)
-explorer.on_event(PanEnd, pan_python_callback)
+    dim = 12
+    som, features, names, palette_names = setup_SOM(data_file, dim)
+    som.weights = np.load("Groove-Explorer-2/SOM_Weights_MLR_3M.npy")
 
-text_input = TextInput(value="1000", title="Number of iterations (Press Enter to generate):")
-text_input.on_change("value", text_input_handler)
+    groove_map_info = pd.DataFrame(get_winners(features, names, som, palette_names), columns=['GrooveName',
+                                                                                              'PaletteName', 'X', 'Y',
+                                                                                              'Colour'])
+    source = ColumnDataSource(groove_map_info)
+
+    hover = HoverTool()
+    hover.tooltips = [
+        ('Name', '@GrooveName'),
+        ('Palette', '@PaletteName'),
+    ]
+
+    TOOLS = "crosshair, wheel_zoom"
+    explorer = figure(x_range
+                      =(-1, dim), y_range=(-1, dim), tools=TOOLS, title='Groove Explorer 2')
+
+    tap = TapTool()
+    tap.callback = CustomJS(code=PLAY_AUDIO_EXPLORER, args=dict(source=source))
+    explorer.add_tools(hover)
+    explorer.add_tools(tap)
+
+    renderer = explorer.circle(source=source, x='X', y='Y', color='Colour', fill_alpha=0.6, size=15,
+                               hover_fill_color='yellow', hover_alpha=1, nonselection_alpha=0.6)
+
+    if explorer_type == 'Customised':
+        point_drag = PointDrawTool(renderers=[renderer], add=False)
+        explorer.add_tools(point_drag)
+        explorer.on_event(PanEnd, pan_python_callback)
+        go_back_button = Button(label='Undo Customize')
+        go_back_button.on_click(go_back)
+
+        return column(explorer, go_back_button)
+    else:
+        return explorer
 
 
+part_1_explorer = make_explorer(data_file='Part_1_Unweighted', explorer_type='Static')
+explorer_tab = Panel(child=part_1_explorer, title="Part 1 - Groove Explorer Static")
 
-train_button = Button(label='Add 500 trainings')
-def add_trainings():
-    regenerate_SOM(num_iterations=100)
-train_button.on_click(add_trainings)
-
-go_back_button = Button(label='Undo Customize')
-def go_back():
-    som.revert_active_learning()
-    groove_map_info.update(pd.DataFrame(get_winners(features, names, som, palette_names),
-                                                columns=['GrooveName', 'PaletteName', 'X', 'Y', 'Colour']))
-    for i in range(94):
-        new_X = groove_map_info['X'][i]
-        new_Y = groove_map_info['Y'][i]
-        source.patch({'X': [(i, new_X)], 'Y': [(i, new_Y)]})
-
-go_back_button.on_click(go_back)
-
-curdoc().add_root(column(explorer,text_input, train_button, go_back_button))
+part_2_explorer = make_explorer(data_file='Part_2_Unweighted', explorer_type='Customised')
+customised_explorer_tab = Panel(child=part_2_explorer
+                                , title="Part 2 - Customised Groove Explorer")
+tabs = Tabs(tabs=[explorer_tab, customised_explorer_tab])
+curdoc().add_root(tabs)
